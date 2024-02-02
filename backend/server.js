62f08dde5d2428a07f22db55315dbe3562b6e507
spyder5658 <thesecretengineers007@gmail.com>
2024-02-02 21:20:08 +0545

    included the signup functionality and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ require("dotenv").config()
 const mongoose = require('mongoose')
 
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
-const app = express()
-
+app=express()
 app.use(express.json())
 
 app.use((req,res,next)=>{
@@ -16,6 +16,7 @@ app.use((req,res,next)=>{
 
 //Routes
 app.use("/api/workouts",workoutRoutes)
+app.use("/api/user",userRoutes)
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI).then(()=>{
