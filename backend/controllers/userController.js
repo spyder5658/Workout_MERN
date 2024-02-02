7d9d39950f3f6f17a81a1ec3349584b42e838454
spyder5658 <thesecretengineers007@gmail.com>
2024-02-02 22:08:16 +0545

    updated the login mechanism

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,21 @@ const creatToken =(_id)=>{
 
 //login user
 const loginUser = async(req,res)=>{
-    res.json({mssg:'login user'})
+    // res.json({mssg:'login user'})
+    const {email,password} = req.body 
+    try{
+        const user = await User.login(email,password)
+
+        //create token
+        const token = creatToken(user._id)
+
+        res.status(200).json({email,token})
+
+    }catch(error){
+        res.status(400).json({error:error.message})
+
+    }
+
 
 }
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -42,5 +42,21 @@ userSchema.statics.signup = async function(email,password){
     
 }
 
+//static login method
+userSchema.statics.login = async function(email,password){
+    if(!email || !password){
+        throw Error('All fields must be filled!!')
+    }
+    const user = await this.findOne({email})
+    if (!user){
+        throw Error('Incorrect Email!')
+    }
+    const match = await bcrypt.compare(password,user.password)
+    if(!match){
+        throw Error('Incorrect password')
+    }
+    return user
+}
+
 
 module.exports = mongoose.model('User',userSchema)
