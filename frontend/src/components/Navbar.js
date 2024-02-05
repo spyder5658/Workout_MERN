4e05a34905481c0dd12c61e9230c630e3bc012ab
spyder5658 <thesecretengineers007@gmail.com>
2024-02-05 22:50:47 +0545

    set the user AuthContext

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,31 +1,37 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 const Navbar = () => {
-    const {logout} = useLogout()
-    const handleClick=()=>{
-        logout()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+  };
 
-    }
-
-
-    return (  
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1>Workout Buddy</h1>
-                </Link>
-                <nav>
-                    <div>
-                        <button onClick={handleClick}>Log out</button>
-                    </div>
-                    <div>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/signup'>SignUp</Link>
-                    </div>
-                </nav>
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
             </div>
-        </header>
-    );
-}
- 
+          )}
+
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">SignUp</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
 export default Navbar;
